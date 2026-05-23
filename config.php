3099c6217cd2566fbaef607ad195577796b75f05
piornos <piornosluthier@gmail.com>
2026-05-23 15:36:16 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Datos de configuración (Sustituye con tus datos reales)
-define('DB_HOST', 'sql111.ezyro.com');
-define('DB_USER', 'ezyro_41927587');
-define('DB_PASS', '06976c5584'); // Aquí queda la clave "escondida"
-define('DB_NAME', 'ezyro_41927587_supositorio');
